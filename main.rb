c04ab74d80c0359434658be8cdb386da15d3b5f4
Sergey Sergyenko <sergey.sergyenko@gmail.com>
2017-01-26 18:50:16 +0300

    Added basic implementaion of linear approach form the paper

--- main.rb
+++ main.rb
@@ -1,57 +1,86 @@
+# This is implementation of Linear Model and basic approach described in:
+# http://mpref2012.lip6.fr/proceedings/RoosMPREF2012.pdf
+#
+# Consider the data in Table 1. There are five reviewers
+# (ri) and five papers (pj). The original scores yi j from the reference are here
+# multiplied by 10 and are thus in the range from 0 to 10. Consisting
+# of only 15 scores in total, this data set is very small
+
 require 'csv'
 
-# classical statistical approach from the field of linear models
-# to uses the analysis of variance and can thus be realized by means
-# of existing statistical software.
+reviews = [
+  [ 6 , 6 , 6 ,'-','-'],
+  [ 3 ,'-','-', 4 ,'-'],
+  [ 3 ,'-','-','-', 4 ],
+  ['-', 3 , 3 , 4 , 4 ],
+  ['-', 3 , 3 , 4 , 4 ],
+].freeze
 
-class BiasedReviewer
-  # Load data file, that is placed in the same directory
-  # named data.csv with ',' symbol used as a delimiter
-  def initialize
-    begin
-      @data = CSV.read('./data.csv')
-    rescue Exception => e
-      puts e.message
-      puts e.backtrace.inspect
-    end
+puts 'Initial reviews:'
+
+reviews.each do |row|
+  puts row.inspect
+end
+
+papers = reviews.first.size
+reviewers = reviews.size
+
+puts "\nData set size is #{reviewers} x #{papers}"
+
+def get_mu(reviews)
+  elements = reviews.dup.flatten
+  count_mean(elements)
+end
+
+def alpha(r, mu, reviews)
+  elements = reviews[r-1].dup
+  count_mean(elements) - mu
+end
+
+def beta(p, mu, reviews)
+  elements = reviews.map { |row| row[p-1] }
+  count_mean(elements) - mu
+end
+
+def count_mean(elements)
+  elements.delete_if { |x| x == '-' }
+  elements.reduce(0,:+)/elements.size.to_f
+end
+
+def y(alpha,beta,mu)
+  mu + alpha + beta
+end
+
+# count mu
+mu = get_mu(reviews)
+puts "mu: #{mu}"
+
+# count alphas
+a = []
+reviewers.times do |i|
+  a << alpha(i+1, mu, reviews)
+end
+puts "alphas: #{a}"
+
+# count betas
+b = []
+papers.times do |i|
+  b << beta(i+1, mu, reviews)
+end
+puts "betas: #{b}"
+
+# calculating y[i,j]
+# yi j = µ +αi +βj +εij
+# εij considered as 0 for this calculation
+y_array = []
+reviewers.times do |i|
+  y_array[i] = []
+  papers.times do |j|
+    y_array[i] << reviews[i][j] == '-' ? '-' : y(a[i], b[j], mu)
   end
+end
 
-  # This is a placeholder for parsing data algorythm
-  def parse_data
-    @data
-  end
-
-  # Here we're going to aggregate all the statistics results
-  def calculate_result
-    while (@data) do |i,j|
-      linear_model(i,j)
-    end
-  end
-
-  private
-
-  # The strategy in the following is to ignore the discretization in the
-  # statistics and to assume that the discretized data belong to the truly
-  # linear model
-
-  def linear_model(i,j)
-    y = µ + α(i) + β(j) + ε(i,j)
-  end
-
-  def µ()
-    # µ is the overall mean of all scores given
-  end
-
-  def α(i)
-    # α(i) is the mean difference between the scores of reviewer r(i) and µ
-  end
-
-  def β(i,j)
-    # β(j) is the mean difference between the scores of paper p(j) and µ
-  end
-
-  def ε(i,j)
-    # ε(i,j) is a random error for (i,j)
-  end
-
+puts "\nRecalculated reviews:"
+y_array.each do |row|
+  puts row.inspect
 end
